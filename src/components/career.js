0a5262f34610119d0578b03f57b4d3fea262687f
Somanshu <somanshu63@gmail.com>
2024-05-01 12:33:57 +0530

    responsive fixes

--- src/components/career.js
+++ src/components/career.js
@@ -1,9 +1,9 @@
 function Career() {
   return (
     <div>
-      <div className="greenbg pLR67">
+      <div className="greenbg pLR67 mRT3">
         <div className=" py-32 px-20">
-          <p className="fs-36 white leading-tight mb-12">
+          <p className="fs-36 white leading-tight mb-12 ">
             Make your move.<br></br> Join our team.
           </p>
           <p className="fs-17 white  leading-6">

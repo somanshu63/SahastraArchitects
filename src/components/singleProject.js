e33ede82c285b6e6716f67b9954ae4d47f180afc
Somanshu <somanshu63@gmail.com>
2024-05-02 16:30:54 +0530

    top fixes

--- src/components/singleProject.js
+++ src/components/singleProject.js
@@ -10,7 +10,8 @@ function SingleProject(props) {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+
     projectsArray.filter((project) => {
       project.projects.filter(
         (project) => project.slug === projectSlug && setProject(project)
@@ -28,50 +29,54 @@ function SingleProject(props) {
       {loading ? (
         <div class="load"></div>
       ) : (
-        <>
-          <img
-            src={project?.image[currentImageIndex]}
-            className="w-full h-60 "
-            alt={project.alt}
-          ></img>
-          {project?.image.length > 1 ? (
-            <div className="flex mt-4 float-right">
-              <RiArrowLeftSLine
-                className="pointer"
-                onClick={() => {
-                  setCurrentImageIndex(
-                    currentImageIndex === 0
-                      ? project.image.length - 1
-                      : currentImageIndex - 1
-                  );
-                }}
-                style={{
-                  fontSize: "1.875rem",
-                  marginRight: "10px",
-                }}
-              />
-              <RiArrowRightSLine
-                className="pointer"
-                onClick={() => {
-                  setCurrentImageIndex(
-                    project.image.length - 1 === currentImageIndex
-                      ? 0
-                      : currentImageIndex + 1
-                  );
-                }}
-                style={{
-                  fontSize: "1.875rem",
-                }}
-              />
-            </div>
-          ) : (
-            ""
-          )}
-          <h3 className="mt-32 fs-27">{project?.name}</h3>
-          <p className="fs-17 mt-8 gray">{project.location}</p>
-          <p className="fs-17 mt-8 gray">{project.size}</p>
-          <p className="fs-17 mt-8 gray">{project.year}</p>
-        </>
+        <div>
+          <div>
+            <img
+              src={project?.image[currentImageIndex]}
+              className="w-full h-60 "
+              alt={project.alt}
+            ></img>
+            {project?.image.length > 1 ? (
+              <div className="flex mt-4 float-right">
+                <RiArrowLeftSLine
+                  className="pointer"
+                  onClick={() => {
+                    setCurrentImageIndex(
+                      currentImageIndex === 0
+                        ? project.image.length - 1
+                        : currentImageIndex - 1
+                    );
+                  }}
+                  style={{
+                    fontSize: "1.875rem",
+                    marginRight: "10px",
+                  }}
+                />
+                <RiArrowRightSLine
+                  className="pointer"
+                  onClick={() => {
+                    setCurrentImageIndex(
+                      project.image.length - 1 === currentImageIndex
+                        ? 0
+                        : currentImageIndex + 1
+                    );
+                  }}
+                  style={{
+                    fontSize: "1.875rem",
+                  }}
+                />
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+          <div>
+            <h3 className="mt-32 fs-27">{project?.name}</h3>
+            <p className="fs-17 mt-8 gray">{project.location}</p>
+            <p className="fs-17 mt-8 gray">{project.size}</p>
+            <p className="fs-17 mt-8 gray">{project.year}</p>
+          </div>
+        </div>
       )}
     </div>
   );
--- src/components/studios.js
+++ src/components/studios.js
@@ -1,50 +1,51 @@
-import react, { useRef, useState, Component } from "react";
+import react, { useRef, useState, Component, useEffect } from "react";
 import DoonOffice2 from "../images/home/DoonOffice2.jpg";
 import haridwarOffice from "../images/home/DoonOffice3.jpg";
 import MapWithMarkers from "./map";
 
-class Studios extends Component {
-  render() {
-    return (
-      <div className="pLR67 mRT3">
-        <MapWithMarkers />
+function Studios() {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+  return (
+    <div className="pLR67 mRT3">
+      <MapWithMarkers />
 
-        {window.innerWidth < 550 ? (
-          <p className="fs-36 fw-700 px-20 leading-tight mt-32">
-            With studio cultures unique to their place and people, we’re the
-            smallest “big firm” out there.
-          </p>
-        ) : (
-          <p className="fs-36 fw-700 px-20 leading-tight mt-32">
-            With studio cultures unique to <br></br> their place and people,
-            we’re <br></br> the smallest “big firm” out <br></br>
-            there.
-          </p>
-        )}
-        <div className="flex items-center mt-32 mb-16">
-          <div className="line"></div>
-          <p className="ml-7 fs-27  ">Our Studios</p>
-        </div>
-        <ul className="flex items-start">
-          <li className="w-250O mb-12 mr-8">
-            <img
-              src={DoonOffice2}
-              alt="projects"
-              className="w-250I mb-6 h-48"
-            ></img>
-            <p className="text-base font-bold mb-3  leading-7">Dehradun</p>
-          </li>
-          <li className="w-250O mb-12 ">
-            <img
-              src={haridwarOffice}
-              alt="projects"
-              className="w-250I mb-6 h-48"
-            ></img>
-            <p className="text-base font-bold mb-3  leading-7">Haridwar</p>
-          </li>
-        </ul>
+      {window.innerWidth < 550 ? (
+        <p className="fs-36 fw-700 px-20 leading-tight mt-32">
+          With studio cultures unique to their place and people, we’re the
+          smallest “big firm” out there.
+        </p>
+      ) : (
+        <p className="fs-36 fw-700 px-20 leading-tight mt-32">
+          With studio cultures unique to <br></br> their place and people, we’re{" "}
+          <br></br> the smallest “big firm” out <br></br>
+          there.
+        </p>
+      )}
+      <div className="flex items-center mt-32 mb-16">
+        <div className="line"></div>
+        <p className="ml-7 fs-27  ">Our Studios</p>
       </div>
-    );
-  }
+      <ul className="flex items-start">
+        <li className="w-250O mb-12 mr-8">
+          <img
+            src={DoonOffice2}
+            alt="projects"
+            className="w-250I mb-6 h-48"
+          ></img>
+          <p className="text-base font-bold mb-3  leading-7">Dehradun</p>
+        </li>
+        <li className="w-250O mb-12 ">
+          <img
+            src={haridwarOffice}
+            alt="projects"
+            className="w-250I mb-6 h-48"
+          ></img>
+          <p className="text-base font-bold mb-3  leading-7">Haridwar</p>
+        </li>
+      </ul>
+    </div>
+  );
 }
 export default Studios;
